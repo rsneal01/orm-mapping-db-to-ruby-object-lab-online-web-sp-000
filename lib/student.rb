bee5aeb188be9aba573480c24420ccd0a66fc434
Bobby Neal <rsneal01@gmail.com>
2020-09-11 21:51:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,7 +50,15 @@ class Student
   end
   
   def self.first_X_students_in_grade_10(number_of_students)
-    
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT 10 
+    SQL
+ 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
   end
   
   def self.find_by_name(name)
