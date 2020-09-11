336c10e5b599f0308778fa8a1402014ff49b6765
Bobby Neal <rsneal01@gmail.com>
2020-09-11 21:34:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,11 +37,11 @@ class Student
     end
   end
   
-  def self.all_students_in_grade_9
+  def self.all_below_12th_grade
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = 9
+      WHERE grade < 12
     SQL
  
     DB[:conn].execute(sql).map do |row|
