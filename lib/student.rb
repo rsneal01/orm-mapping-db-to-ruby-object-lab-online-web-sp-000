5379f094ae1f2acff6e76325a34a90ee21ad8b6e
Bobby Neal <rsneal01@gmail.com>
2020-09-11 21:18:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,8 +16,9 @@ class Student
       FROM students
     SQL
  
-    DB[:conn].execute(sql)
-  
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
     
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
