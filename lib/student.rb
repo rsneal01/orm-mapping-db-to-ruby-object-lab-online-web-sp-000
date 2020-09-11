af31b17fa9732fc260959d3197741ef3b92927b5
Bobby Neal <rsneal01@gmail.com>
2020-09-11 21:24:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,7 +26,13 @@ class Student
 
 
   def self.all_students_in_grade_9
-    
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9th
+    SQL
+ 
+    DB[:conn].execute(sql)
   end
   
   def self.find_by_name(name)
