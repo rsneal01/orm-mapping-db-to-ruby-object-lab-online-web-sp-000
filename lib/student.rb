410f492dfd8c4c795b124f2b21c829e05523074a
Bobby Neal <rsneal01@gmail.com>
2020-09-11 21:19:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,6 +24,11 @@ class Student
     # remember each row should be a new instance of the Student class
   end
 
+
+  def .all_students_in_grade_9
+    
+  end
+  
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT *
